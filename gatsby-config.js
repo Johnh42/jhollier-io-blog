b00d087a55bbae9f904cf7ea8d011aae6a4d7e99
John Hollier <ulpetie@gmail.com>
2022-09-02 14:27:15 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `SysAdmin jhollier`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `John Hollier`,
+      summary: `who builds and researches useful Systems`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
     },
   },
   plugins: [
